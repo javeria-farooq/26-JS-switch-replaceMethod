2d9738a10d5a3532f79a566e2209b9dd91fbfefa
javeria-farooq <gettingeducation6@gmail.com>
2024-01-25 14:32:49 +0500

    26 JS Assignment

---
+++ app.js
@@ -0,0 +1,27 @@
+// let num = prompt('enter value')
+// switch(num) {
+//     case '5':
+//         document.write('han 5 h');
+//         break
+
+//     case '4':
+//         document.write('han 4 h');
+//         break
+
+//     default:
+//         alert('no')
+// }
+
+// let text = 'Hello world hello world'
+// let text2 = text.replace('world', 'pakistan') 
+
+// ab kia hga mre ps text mn 2 worlds hn or text2 mn mne ise kaha h k world ko pakistan se
+// repalce krdo lkn ye kia krega 1st wle world ko pakistan se replace krdga lkn 2nd wle ko nh
+// krega ab hmn kia krna prega hm is world ko global krnge t ek variable mn jtne bh same words
+// wo globally un sbko replace krdga ab global krne ka treeqa kia h wo ye h k jsko global krna
+// h usko string mn nh lkhnge just wse hi lkh dnge jse hm ksi variable ko dynamically cl krne
+// mn lkhte hn and jsko replace krna h oose string mn nh lkhnge then uske aage peeche fwd slash
+// lgaenge phr rit wle slash k baad g lkh dnge g for globally
+// let text2 = text.replace(/world/g, 'pakistan')
+
+// document.write(text2)
